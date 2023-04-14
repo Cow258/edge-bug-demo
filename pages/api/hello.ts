981a5a917d213d9011aa163884f881d308512b16
Cowboy Ho <dreamran43@gmail.com>
2023-04-14 14:00:36 +0800

    Update hello api for edge

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
-type Data = {
-  name: string
-}
+export const config = {
+  runtime: "edge",
+  regions: ["sin1"],
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextRequest) {
+  const reqHeaders = {} as { [x: string]: string };
+  req.headers.forEach((v, k) => {
+    reqHeaders[k] = v;
+  });
+  console.log("request headers > ", JSON.stringify(reqHeaders, null, 2));
+  return new Response(JSON.stringify({ hello: "world" }), {
+    headers: { "content-type": "application/json" },
+  });
 }
